44504b0b12218198fb960bdfc850e3c3c06572c1
Reileen00 <business.rout001@gmail.com>
2025-08-13 16:00:36 +0530

    1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Reileen00/blockchain
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Server
+// Transport => tcp , udp ,
+// Block
+// Transaction
+// Key pair
+
+func main() {
+	fmt.Println("hello")
+}
