d335dc25ed23715fdb017631abaf92bc653cf530
Reileen00 <business.rout001@gmail.com>
2025-08-15 03:00:44 +0530

    5

--- core/block_test.go
+++ core/block_test.go
@@ -19,8 +19,17 @@ func randomBlock(height uint32) *Block {
 	tx := Transaction{
 		Data: []byte("foo"),
 	}
+
 	return NewBlock(header, []Transaction{tx})
 }
+
+func randomBlockWithSignature(t *testing.T, height uint32) *Block {
+	privKey := crypto.GeneratePrivateKey()
+	b := randomBlock(height)
+	assert.Nil(t, b.Sign(privKey))
+	return b
+}
+
 func TestSignBlock(t *testing.T) {
 	privKey := crypto.GeneratePrivateKey()
 	b := randomBlock(0)
